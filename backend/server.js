d2e8e76920f14635c50cd1359167fb01df1ea73e
light-world123 <urhukpeejiyere@gmail.com>
2024-05-04 03:36:30 +0100

    Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log('Server is Running');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT||5000
+
+const app = express ()
+
+app.listen(PORT, ()=> console.log(`Server stated on port ${PORT}`))
+
+app.get('/', (req, res)=>{
+    // res.send('Hello')
+    res.status(200).json({message: 'Welcome to Support Desk API'})
+})
+
+// Fetched all users
+app.get('/api/users', (req, res)=>{
+    // res.send('Hello')
+    res.status(200).json({message: 'All users fetched'})
+})
+
+// Sign up a new user
+app.post('/api/users', (req, res)=>{
+    res.status(201).json({message: 'New User Created'})
+})
